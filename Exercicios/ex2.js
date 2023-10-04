66ab4501c79d79dbbc03c63594be2a93719b85ee
Mustafa Eideh <mustafaeideh100@outlook.com>
2023-10-04 18:42:51 -0300

    exercício 2

---
+++ Exercicios/ex2.js
@@ -0,0 +1,19 @@
+/**
+ * Frase a ser montada:
+ * 
+ * "Seu nome" tem "sua idade" anos, pesa "seu peso" Kg
+ * tem "sua altura" de altura e seu IMC é de "seu IMC".
+*/
+
+let nome = 'Mustafa';
+let idade = 20;
+let peso = 91;
+let altura = 1.76;
+let imc;
+imc = peso / altura;
+
+
+
+
+
+console.log(nome, ' tem ', idade ,' anos, pesa ', peso, ' Kg e tem ', altura,' de altura e seu IMC é de ', imc, '.')
