5e01e21b75495c1f61eba8c0d4fe81272b386b7f
Mustafa Eideh <mustafaeideh100@outlook.com>
2023-10-05 19:42:28 -0300

    ex3

---
+++ Exercicios/ex3.js
@@ -0,0 +1,9 @@
+/**
+ * Retorne uma conta com duas váriaveis escolhidas pelo usuário.
+ */
+
+
+let num1 = prompt('Digite um valor:');
+let num2 = prompt ('Digite outro valor:');
+let resultado = Number(num1) + Number(num2);
+alert(resultado);
